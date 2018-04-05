a183564bc597a2d907997b9235461cf5f98b5a8d
a-yu-relativity <ayu@kcura.com>
2018-04-05 10:42:36 -0500

    Inital commit

---
+++ MassEditPermissions/Methods.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kCura.Relativity.Client;
+using kCura.Relativity.Client.DTOs;
+using Relativity.Services.Group;
+using Relativity.Services.Permission;
+
+namespace MassEditPermissions
+{
+    public static class Methods
+    {
+        public static List<int> GetAllWorkspaceIds(IRSAPIClient proxy)
+        {
+            List<int> workspaceIds = new List<int>();
+            proxy.APIOptions.WorkspaceID = -1;
+
+            var query = new kCura.Relativity.Client.DTOs.Query<Workspace>
+            {
+                Condition = new kCura.Relativity.Client.WholeNumberCondition("Artifact ID", NumericConditionEnum.IsSet)
+            };
+
+            // should be less than 10000 results
+            QueryResultSet<Workspace> resultSet = proxy.Repositories.Workspace.Query(query, 0);
+            if (resultSet.Success)
+            {
+                workspaceIds.AddRange(resultSet.Results.Select(result => result.Artifact.ArtifactID));
+            }
+
+            return workspaceIds;
+        }
+
+        public static async Task<bool> DisableAddDocInWorkspaceForGroups(
+            IPermissionManager mgr, 
+            int workspaceId,
+            ISet<int> groupIds)
+        {
+            // get group selector for workspace
+            GroupSelector sel;
+            try
+            {
+                sel = await mgr.GetWorkspaceGroupSelectorAsync(workspaceId);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            foreach (GroupRef group in sel.EnabledGroups)
+            {
+                // see if the group is one whose permissions
+                // we would like to disable
+                if (groupIds.Contains(group.ArtifactID))
+                {
+                    GroupPermissions permissions = await mgr.GetWorkspaceGroupPermissionsAsync(workspaceId, group);
+                    // get object permissions
+                    List<ObjectPermission> objPermissions = permissions.ObjectPermissions;
+
+                    // get the document permission
+                    ObjectPermission docPerm = objPermissions.FirstOrDefault(x => x.Name == "Document");
+                    if (docPerm != null && docPerm.AddSelected)
+                    {
+                        // disable Add permission
+                        docPerm.AddSelected = false;
+                    }
+
+                    // make changes
+                    try
+                    {
+                        await mgr.SetWorkspaceGroupPermissionsAsync(workspaceId, permissions);
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Disables the "add document" permission for specified workspaces and group combinations
+        /// </summary>
+        /// <param name="mgr">Permission manager service</param>
+        /// <param name="workspaceIds">The list of workspace IDs that we want to do this for</param>
+        /// <param name="groupIds">The artifact IDs of the groups whose add document permission we want to revoke</param>
+        /// <returns></returns>
+        public static async Task<bool> DisableAddDocAsync(IPermissionManager mgr, ISet<int> workspaceIds, ISet<int> groupIds)
+        {
+            bool success = false;
+
+            foreach (int workspaceId in workspaceIds)
+            {
+                success = await DisableAddDocInWorkspaceForGroups(mgr, workspaceId, groupIds);
+                // if we failed, break
+                if (!success)
+                {
+                    break;
+                }
+            }
+
+            return success;
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currDir = Environment.CurrentDirectory;
+            string groupsFilePath = "";
+            string[] groupsIdsStr = File.ReadAllLines(groupsFilePath);
+
+            // store in hash set for faster inclusion testing
+            HashSet<int> groupIds = new HashSet<int>();
+            foreach (var groupStr in groupsIdsStr)
+            {
+                int groupId = Int32.Parse(groupStr);
+                groupIds.Add(groupId);
+            }
+
+
+        }
+    }
+}
